70d0a6596fdccdffbb4c82b9a20b47859fdf4da5
Zer0Bytes <25849503+DataCluster0@users.noreply.github.com>
2025-02-22 21:01:15 +0200

    0.1.3

--- QuirrelDoc.h
+++ QuirrelDoc.h
@@ -219,6 +219,19 @@ bool Sleep(uint32 MilliSeconds);
 /// @return Was the key pressed
 bool IsKeyPressed(string KeyName);
 
+///////////////////////////////////////////// Modules
+
+/// @return Is the module currently loaded?
+bool IsModuleLoaded(string ModuleName);
+
+/// @return Returns the defined 'Version' of a module
+uint32 GetModuleVersion(string ModuleName);
+
+/// @return Returns the defined 'VersionString' of a module
+string GetModuleVersionString(string ModuleName);
+
+/////////////////////////////////////////////
+
 /* KeyNames
 0 1 2 3 4 5 6 7 8 9 A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
 - = , . ; / ` [ \ ] ' F1 F2 F3 F4 F5 F6 F7 F8 F9 F10 F11 F12
